60f407f4ca5c2a1a0473dd56859cf35a6edc0bdd
BozinovsiDaniel <Bozinovskidaniel@hotmail.com>
2019-09-13 00:42:44 +1000

    latest

--- getflix/src/components/view.js
+++ getflix/src/components/view.js
@@ -1,32 +1,51 @@
 import React, {Component} from 'react';
 import MovieRow from '../js/MovieRow';
+import $ from 'jquery';
 
 class View extends Component {
     constructor(props) {
         super(props);
+        this.state = {};
+        this.performSearch("it");
+    }
 
-        const movies = [
-            {id: 0, poster_src:"https://images.unsplash.com/photo-1518568814500-bf0f8d125f46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80", title: 'some lorem iasd', overview: "some text about idk ahwat im on about"},
-            {id: 1, poster_src:"https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg", title: 'Avengers', overview: "2ns overview"}
-        ]
+    performSearch(searchTerm) {
 
-        var movieRows = [];
-        movies.forEach((movie) => {
-            console.log(movie.title);
-            const movieRow = <MovieRow movie={movie}/>
-            
-            movieRows.push(movieRow);
+        const urlStr = "https://api.themoviedb.org/3/search/multi?api_key=d31bf19d99f4cca10b2a1aa31b7abeaf&language=en-US&query=" + searchTerm;
+        $.ajax({
+            url: urlStr,
+            success: (searchResults) => {
+                console.log("fetched data success");
+                const results = searchResults.results;
+                console.log(results);
+                
+                var movieRows = [];
 
+                results.forEach((movie) => {
+                    movie.poster_src = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
+                    const movieRow = <MovieRow key={movie.id} movie={movie} />
+                    movieRows.push(movieRow);
+                })
+                
+                this.setState({rows: movieRows});
+            },
+            error: (xhr, status, err) => {
+                console.log("failed");
+            }
         })
+    }
 
-        this.state = {rows: movieRows}
+    searchChangeHandler(event) {
+        console.log(event.target.value);
+        const searchTerm = event.target.value;
+        this.performSearch(searchTerm);
     }
 
     render() {
         return (
             <div>
 
-                <input className="searchbar" placeholder="Enter search term"></input>
+                <input className="searchbar" placeholder="Enter search term" onChange={this.searchChangeHandler.bind(this)}></input>
                 {this.state.rows}
 
 
--- getflix/src/js/MovieRow.js
+++ getflix/src/js/MovieRow.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 
 class MovieRow extends Component {
+    viewMovie() {
+        const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+        window.location.href = url;
+    }
+
     render() {
         return <table key={this.props.movie.id}>
         <tbody>
             <tr>
-                <td>
-                    <img className="posters" src={this.props.movie.poster_src} width="100px" alt="poster" />
+                <td width="20%">
+                    <img className="posters" src={this.props.movie.poster_src} width="250px" alt="poster" />
                 </td>
-                <td>
-                    {this.props.movie.title}
+                <td width="60%">
+                    <h3>{this.props.movie.title}</h3>
                     <p>{this.props.movie.overview}</p>
+                    <input type="button" onClick={this.viewMovie.bind(this)} value="View" />
+                </td>
+                <td width="10%">
+                    <p>Rating: {this.props.movie.vote_average}</p>
+                </td>
+                <td width="10%">
+                    <p>Release Date: {this.props.movie.release_date}</p>
                 </td>
 
             </tr>
