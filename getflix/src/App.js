34b88694886d8e2ed1c090458b06cc73c62a1459
Daniel Bozinovski <BozinovskiDaniel@hotmail.com>
2019-09-12 17:30:32 +1000

    latest

---
+++ getflix/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Nav from './components/nav';
+import View from './components/view';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <View />
+    </div>
+  );
+}
+
+export default App;
---
+++ getflix/src/components/view.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function View() {
+  return (
+    <div>
+        <input className="searchbar" placeholder="Enter search term"></input>
+    </div>
+  );
+}
+
+export default View;
