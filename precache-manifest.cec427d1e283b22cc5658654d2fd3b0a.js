0b8e483ac8c36cb7898e1177f73506ed574bf3bb
BozinovsiDaniel <Bozinovskidaniel@hotmail.com>
2020-04-18 00:43:05 +1000

    Updates

---
+++ precache-manifest.cec427d1e283b22cc5658654d2fd3b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ce3bc8ca7f988a61607882f1ef32fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca670dab879e030ea2e0",
+    "url": "/static/css/main.81165b6e.chunk.css"
+  },
+  {
+    "revision": "b724a204c8453bdfb957",
+    "url": "/static/js/2.28eb7551.chunk.js"
+  },
+  {
+    "revision": "ca670dab879e030ea2e0",
+    "url": "/static/js/main.83fea44c.chunk.js"
+  },
+  {
+    "revision": "e4509bf5bb5b96a829e4",
+    "url": "/static/js/runtime~main.c44ca3b4.js"
+  },
+  {
+    "revision": "516ef41e4eb8990cb861ad53a2c5800a",
+    "url": "/static/media/logo.516ef41e.png"
+  }
+]);
